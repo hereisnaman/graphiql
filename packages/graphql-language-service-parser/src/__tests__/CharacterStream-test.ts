78134122d18b12e844005acaf133cb3cfc580b4d
Naman Kumar <naman@outlook.in>
2020-07-09 14:39:26 +0530

    [Parser] Update tests to support lexer migration]

--- packages/graphql-language-service-parser/src/__tests__/CharacterStream-test.ts
+++ packages/graphql-language-service-parser/src/__tests__/CharacterStream-test.ts
@@ -7,6 +7,11 @@ describe('CharacterStream', () => {
       const stream = new CharacterStream(source);
 
       expect(stream.getStartOfToken()).toEqual(0);
+
+      stream.advance();
+      stream.advance();
+
+      expect(stream.getStartOfToken()).toEqual(7);
     });
   });
 
@@ -17,9 +22,9 @@ describe('CharacterStream', () => {
 
       expect(stream.getCurrentPosition()).toEqual(0);
 
-      stream.next();
+      stream.advance();
 
-      expect(stream.getCurrentPosition()).toEqual(1);
+      expect(stream.getCurrentPosition()).toEqual(6);
     });
   });
 
@@ -30,7 +35,7 @@ describe('CharacterStream', () => {
 
       expect(stream.sol()).toEqual(true);
 
-      stream.next();
+      stream.advance();
 
       expect(stream.sol()).toEqual(false);
     });
@@ -43,222 +48,10 @@ describe('CharacterStream', () => {
 
       expect(stream.eol()).toEqual(false);
 
-      stream.skipToEnd();
+      stream.advance();
+      stream.advance();
 
       expect(stream.eol()).toEqual(true);
-    });
-  });
-
-  describe('peak', () => {
-    it('returns the next character in source string', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream.peek()).toEqual('s');
-
-      stream.next();
-
-      expect(stream.peek()).toEqual('c');
-    });
-
-    it('returns null if at end of the source string', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.skipToEnd();
-
-      expect(stream.peek()).toEqual(null);
-    });
-  });
-
-  describe('next', () => {
-    it('increments the current position', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.next();
-
-      expect(stream.getCurrentPosition()).toEqual(1);
-    });
-
-    it('returns the next character in source string', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream.next()).toEqual('s');
-      expect(stream.next()).toEqual('c');
-    });
-  });
-
-  describe('_testNextCharacter', () => {
-    it('tests next character with the pattern provided', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream._testNextCharacter('s')).toEqual(true);
-
-      stream.next();
-
-      expect(stream._testNextCharacter('c')).toEqual(true);
-    });
-
-    it('returns correct result with regex pattern', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream._testNextCharacter(/s/)).toEqual(true);
-    });
-
-    it('returns correct result with a callback function as pattern', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      const matcher = nextChar => nextChar === 's';
-
-      expect(stream._testNextCharacter(matcher)).toEqual(true);
-    });
-  });
-
-  describe('eat', () => {
-    it('returns next character is matched with the pattern provided', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream.eat('s')).toEqual('s');
-    });
-
-    it('returns undefined is not matched with the pattern provided', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream.eat('b')).toEqual(undefined);
-    });
-
-    it('increments current position after successful match', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.eat('s');
-
-      expect(stream.getCurrentPosition()).toEqual(1);
-    });
-
-    it('sets start to current position after successful match', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.next();
-      stream.eat('c');
-
-      expect(stream.getStartOfToken()).toEqual(1);
-    });
-  });
-
-  describe('eatWhile', () => {
-    it('returns true if any character matched', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream.eatWhile(/[_A-Za-z]+/)).toEqual(true);
-    });
-
-    it('returns false if no character matched', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream.eatWhile(/^(?:,)/)).toEqual(false);
-    });
-
-    it('increments current position upto where the characters are matched', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.eatWhile(/[_A-Za-z]+/);
-
-      expect(stream.getCurrentPosition()).toEqual(6);
-      expect(stream.peek()).toEqual(' ');
-    });
-
-    it('sets start to first matched character position', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.skipTo(7);
-      stream.eatWhile(/[_A-Za-z]+/);
-
-      expect(stream.getStartOfToken()).toEqual(7);
-    });
-  });
-
-  describe('eatSpace', () => {
-    it('returns true if space found', () => {
-      const source = '  scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream.eatSpace()).toEqual(true);
-    });
-
-    it('returns false if no space found', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream.eatSpace()).toEqual(false);
-    });
-
-    it('increments current position upto where the space is matched', () => {
-      const source = '  scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.eatSpace();
-
-      expect(stream._pos).toEqual(2);
-    });
-
-    it('sets start to last space character position', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      expect(stream._start).toEqual(0);
-
-      stream._pos = 6;
-      stream.eatSpace();
-
-      expect(stream._start).toEqual(6);
-    });
-  });
-
-  describe('skipToEnd', () => {
-    it('sets pos to end of the source string', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.skipToEnd();
-
-      expect(stream._pos).toEqual(10);
-    });
-  });
-
-  describe('skipTo', () => {
-    it('sets pos to the provided index', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.skipTo(2);
-
-      expect(stream._pos).toEqual(2);
-    });
-  });
-
-  describe('backUp', () => {
-    it('goes back the number of steps provided', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream._pos = 2;
-
-      stream.backUp(1);
-
-      expect(stream._pos).toEqual(1);
     });
   });
 
@@ -267,9 +60,9 @@ describe('CharacterStream', () => {
       const source = 'scalar Foo';
       const stream = new CharacterStream(source);
 
-      stream._pos = 1;
+      stream.advance();
 
-      expect(stream.column()).toEqual(1);
+      expect(stream.column()).toEqual(6);
     });
   });
 
@@ -278,10 +71,9 @@ describe('CharacterStream', () => {
       const source = 'scalar Foo';
       const stream = new CharacterStream(source);
 
-      stream._start = 1;
-      stream._pos = 6;
+      stream.advance();
 
-      expect(stream.current()).toEqual('calar');
+      expect(stream.current()).toEqual('scalar');
     });
   });
 
@@ -308,67 +100,43 @@ describe('CharacterStream', () => {
     });
   });
 
-  describe('match', () => {
-    it('returns false if no match found', () => {
+  describe('advance', () => {
+    it('returns correct matches', () => {
       const source = 'scalar Foo';
       const stream = new CharacterStream(source);
 
-      expect(stream.match('a')).toEqual(false);
+      expect(stream.advance()).toEqual({ kind: 'Name', value: 'scalar' });
+      expect(stream.advance()).toEqual({ kind: 'Name', value: 'Foo' });
     });
 
-    it('returns matches if found', () => {
+    it('returns null when at eol', () => {
       const source = 'scalar Foo';
       const stream = new CharacterStream(source);
 
-      expect(stream.match(/scalar/)[0]).toEqual('scalar');
+      stream.advance();
+      stream.advance();
+
+      expect(stream.advance()).toEqual(null);
     });
 
-    it('accepts string pattern', () => {
+    it('updates start position', () => {
       const source = 'scalar Foo';
       const stream = new CharacterStream(source);
 
-      expect(stream.match('scalar')).toEqual(true);
+      stream.advance();
+      stream.advance();
+
+      expect(stream.getStartOfToken()).toEqual(7);
     });
 
-    it('dose case-insensitve match when caseFold is true', () => {
+    it('updates current position', () => {
       const source = 'scalar Foo';
       const stream = new CharacterStream(source);
 
-      expect(stream.match('Scalar', true, true)).toEqual(true);
-      expect(stream.match('Scalar', true, false)).toEqual(false);
-    });
+      stream.advance();
+      stream.advance();
 
-    it('consumes the source string if a match is found, by default', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.next();
-      stream.match('calar');
-
-      expect(stream.getStartOfToken()).toEqual(1);
-      expect(stream.getCurrentPosition()).toEqual(6);
-    });
-
-    it('does not consumes the source string if no match is found', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.next();
-      stream.match(/123/);
-
-      expect(stream.getStartOfToken()).toEqual(0);
-      expect(stream.getCurrentPosition()).toEqual(1);
-    });
-
-    it('does not consume the source string if marked as false', () => {
-      const source = 'scalar Foo';
-      const stream = new CharacterStream(source);
-
-      stream.next();
-      stream.match('calar', false);
-
-      expect(stream.getStartOfToken()).toEqual(0);
-      expect(stream.getCurrentPosition()).toEqual(1);
+      expect(stream.getCurrentPosition()).toEqual(10);
     });
   });
 });
